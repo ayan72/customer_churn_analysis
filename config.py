2ad8b0c85700975856f7b76238ec9bffa6bbfc32
Ayan <aabbas@ualberta.ca>
2025-07-24 10:44:46 +0530

    connected db; imported tables as df

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+PGUSER = "postgres"              # From the 'Username' field
+PGPASSWORD = "test"     # The one you typed (replace this string)
+PGHOST = "localhost"             # From the 'Host' field
+PGPORT = "5434"                  # IMPORTANT: Your port is 5434, not the default 5432
+PGDATABASE = "postgres"          # From the 'Database' field
